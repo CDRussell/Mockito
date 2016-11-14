561daeff2975e77f073c19709a3a384f9abb2dd3
Craig Russell <craig.russell.1986@gmail.com>
2016-11-14 00:17:02 +0000

    update UserRegistration to have two constructors
    demonstration of the difference between dependency injection and a class having to create its own dependencies
    * one constructor which is 0-args, and creates its own database implementation
    * one constructor which receives the database it is to use as a parameter

--- app/src/main/java/io/caster/mockito/UserRegistration.java
+++ app/src/main/java/io/caster/mockito/UserRegistration.java
@@ -4,6 +4,19 @@ public class UserRegistration {
 
     private Database database;
 
+    /* this does not use Dependency Injection;
+     *     - this class is tied to SimpleDatabase implementation
+     *     - no other implementation of a Database can be provided, even during unit testing
+     */
+    public UserRegistration() {
+        this.database = new SimpleDatabase();
+    }
+
+    /* this uses Dependency Injection;
+     *     - the database is given to this method
+     *     - this method doesn't have to worry about fetching the database reference
+     *     - alternative database implementations can be provided
+     */
     public UserRegistration(Database database) {
         this.database = database;
     }
